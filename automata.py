d5c25a8451ab99ff26113b4437afaa48ab12772c
Zazai840 <zazenquiries@gmail.com>
2026-09-15 12:54:54 +0100

    Create Three Potential Cellular Automata

--- automata.py
+++ automata.py
@@ -12,3 +12,17 @@ class Automaton:
                                 apply_rule=lambda n, c, t: cpl.nks_rule(n, rule))
 
         return automaton
+
+    def choose_automaton(self, rule):
+        match rule:
+            case 30: 
+                return self.create(30)
+            case 50:
+                return self.create(50)
+            case 70:
+                return self.create(70)
+            case _:
+                raise ValueError("Input must be 30, 50, or 70.")
+
+    
+            
--- rules/rule30.py
+++ rules/rule30.py
@@ -4,4 +4,4 @@ import matplotlib as plt
 rule30_test = cpl.init_simple(200)
 rule30_test = automaton = cpl.evolve(rule30_test, timesteps=100, memoize=True,
                                 apply_rule=lambda n, c, t: cpl.nks_rule(n, 30))
-
+cpl.plot(rule30_test)
